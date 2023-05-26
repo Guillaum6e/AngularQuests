1cdb0f6260bc724f28cef5a88f1b098bb8d66562
GUILLAUME GROS <guillaume.grosm11@gmail.com>
2023-05-26 11:24:59 +0200

    Quest00

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BlockComponent } from './block/block.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    BlockComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
