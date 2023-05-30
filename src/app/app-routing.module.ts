ea254045cad9ceb155c289b1a772323d408b09b6
GUILLAUME GROS <guillaume.grosm11@gmail.com>
2023-05-30 11:03:18 +0200

    Quest04-Routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BlockComponent } from './sign-up/block.component';
+import { UserComponent } from './user/user.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', title: 'Home', component: UserComponent },
+  { path: 'sign-up', title: 'Sign-up', component: BlockComponent },
+  { path: 'user', title: 'User', component: UserComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BlockComponent } from './block/block.component';
+import { BlockComponent } from './sign-up/block.component';
 import { UserComponent } from './user/user.component';
 import { FormsModule } from '@angular/forms';
+import { MenuComponent } from './menu/menu.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     BlockComponent,
-    UserComponent
+    UserComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
