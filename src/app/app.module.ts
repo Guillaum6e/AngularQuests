3123367499a2cb97d9a420d7cc2a49d0d7e0ff05
GUILLAUME GROS <guillaume.grosm11@gmail.com>
2023-06-09 19:33:31 +0200

    Quest06.14-FormControl

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BlockComponent } from './sign-up/block.component';
 import { UserComponent } from './user/user.component';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MenuComponent } from './menu/menu.component';
 import { MyFormComponent } from './my-form/my-form.component';
 
@@ -20,7 +20,8 @@ import { MyFormComponent } from './my-form/my-form.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/my-form/my-form.component.ts
+++ src/app/my-form/my-form.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { passwordValidator } from './ValidationErrors';
 
 @Component({
   selector: 'app-my-form',
@@ -6,18 +8,55 @@ import { Component } from '@angular/core';
   styleUrls: ['./my-form.component.css']
 })
 export class MyFormComponent {
-  model: Order = new Order("", "", "", "");
+
+  myForm = this.fb.group({
+    fullname: [''],
+    email: [''],
+    password: [''],
+    address: this.fb.group({
+      street: [''],
+      city: [''],
+      zipcode: [''],
+    }),
+  });
+
+  // myForm = this.fb.group({
+  //   fullname: ['', [Validators.required]],
+  //   email: ['', [Validators.email, Validators.required]],
+  //   password: ['', [passwordValidator, Validators.required]],
+  //   address: this.fb.group({
+  //     street: ['', Validators.pattern(RegExp('[a-z ]{1,50}/i'))],
+  //     city: ['', Validators.pattern(RegExp('[a-z ]{1,50}/i'))],
+  //     zipcode: ['', Validators.pattern(RegExp('[0-9]{1,5}'))],
+  //   }),
+  // });
+
+  constructor(private fb: FormBuilder) { }
+
+  person: Person = new Person('', '', '', '', '', 0);
 
   onSubmit() {
-    console.log(this.model);
+    if (this.myForm.valid) {
+      const fullname = this.myForm.value.fullname!;
+      const email = this.myForm.value.email!;
+      const password = this.myForm.value.password!;
+      const street = this.myForm.value.address?.street!;
+      const city = this.myForm.value.address?.city!;
+      const zipcode = parseInt(this.myForm.value.address?.zipcode!);
+
+      this.person = new Person(fullname, email, password, street, city, zipcode)
+    }
   }
+
 }
 
-export class Order {
+export class Person {
   constructor(
-    public firstname: string,
-    public lastname: string,
+    public fullname: string,
     public email: string,
-    public password: string
+    public password: string,
+    public street: string,
+    public city: string,
+    public zipcode: number,
   ) { }
 }
